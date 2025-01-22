03f16b37efb99cecdb41f0ece71ef35ebacedcb6
Badr04-dev <badrmotaim4@gmail.com>
2025-01-22 16:49:16 +0100

    Update JsonFetcher.java

--- app/src/main/java/movies/JsonFetcher.java
+++ app/src/main/java/movies/JsonFetcher.java
@@ -5,23 +5,23 @@ import java.net.URL;
 
 public class JsonFetcher extends AbstractJsonFetcher{
     public static void printNowPlayingMovies() throws Exception {
-        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/movie/now_playing?api_key=8c1f02622cdb942096de14f60750f28b"));
+        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/movie/now_playing?api_key=..."));
     }
 
     public static void printPopularMovies() throws Exception {
-        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/movie/popular?api_key=8c1f02622cdb942096de14f60750f28b"));
+        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/movie/popular?api_key=..."));
     }
 
     public static void printTopRatedMovies() throws Exception {
-        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/movie/top_rated?api_key=8c1f02622cdb942096de14f60750f28b"));
+        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/movie/top_rated?api_key=..."));
     }
 
     public static void printUpcomingMovies() throws Exception{
-        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/movie/upcoming?api_key=8c1f02622cdb942096de14f60750f28b"));
+        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/movie/upcoming?api_key=..."));
     }
 
     public static void printDiscoverMovies() throws Exception{
-        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/discover/movie?api_key=8c1f02622cdb942096de14f60750f28b"));
+        AbstractJsonFetcher.printMenuMovies(new URL("https://api.themoviedb.org/3/discover/movie?api_key=..."));
     }
 }
 
